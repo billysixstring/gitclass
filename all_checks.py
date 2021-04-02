5244f554695c4dbd00267a7b97384cf75081f706
billysixstring <wdupre@nc.rr.com>
2021-04-02 12:06:13 -0400

    Testing staging and committing at once

--- all_checks.py
+++ all_checks.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import os
+import sys
 
 def check_reboot():
 	"""Returns true if the computer has a pending reboot."""
 	return os.path.exists("/run/reboot-required")
 
 def main():
-	pass
+	if (check_reboot()):
+		prit("Pending Reboot")
+		sys.exit(1)
 
 main()
-
