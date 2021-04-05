84eb302c66b6d53a0d42a2b4d479139b61e269f5
billysixstring <wdupre@nc.rr.com>
2021-04-05 15:05:19 -0400

    Added a network check

--- all_checks.py
+++ all_checks.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 import os
 import sys
+import socket
 
 def check_reboot():
 	"""Returns true if the computer has a pending reboot. Added more to this comment from GitHub."""
 	return os.path.exists("/run/reboot-required")
 
+def check_no_network():
+	try:
+		socket.gethostbyname("www.google.com")
+		return False
+	except:
+		return True
+
 def main():
-	if (check_reboot()):
-		print("Pending Reboot")
+	checks=[
+		(check_reboot, "Pending reboot"),
+		(check_no_network, "No working network")
+	]
+
+	everything_ok=True
+
+	for check, msg in checks:
+		if check():
+			print(msg)
+			everything_ok=False
+
+	if not everything_ok:
 		sys.exit(1)
 
 	print("Everything is ok, so don't worry.")
